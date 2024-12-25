673b06ca8eae425e1c00673c75ebea7e1182743f
udaygohel24 <goheluday121@gmail.com>
2024-12-25 12:01:27 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Main from "./Main/Main";
+import APP from "./App.css";
+
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+export default App;
